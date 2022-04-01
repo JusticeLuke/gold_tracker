c25496d3917a76760430ae025625782568ce7e10
Luke Justice <jjusticeluke@gmail.com>
2022-04-01 15:35:10 -0400

    added bootstrap

---
+++ gold_tracker_ui/src/elements/login_form.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export class LoginForm extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="loginForm">
+        <h1>LOGIN FORM</h1>
+        <form>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
