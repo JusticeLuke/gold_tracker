dd7c3d2e32d678cf996927b74c510ea00c09c6ac
Luke Justice <jjusticeluke@gmail.com>
2022-04-11 19:55:15 -0400

    Tweak imports to remove errors

--- gold_tracker_ui/src/App.tsx
+++ gold_tracker_ui/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { LoginForm } from "./components/LoginForm";
 import { Users } from "./components/Users";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 
-let queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
--- gold_tracker_ui/src/components/Users.tsx
+++ gold_tracker_ui/src/components/Users.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Query, useQuery } from "react-query";
+import { useQuery } from "react-query";
 
 export function Users() {
   let { data, isLoading, error } = useQuery("partys", () => {
-    return fetch("http://localhost:8000/partys", {
-      headers: { authorization: "Basic " + window.btoa("admin:Sonic2306") },
+    return fetch("http://127.0.0.1:8000/partys", {
+      headers: { authorization: "Basic" + window.btoa("admin:sonic2306") },
     }).then((response) => response.json());
   });
 
@@ -16,10 +16,9 @@ export function Users() {
       {data ? <DataFormat userData={data} /> : null}
     </div>
   );
-  return null;
 }
 
 function DataFormat(props: any) {
   let username = props.userData.results[0].name;
-  return <p>USERNAME: {username} </p>;
+  return <p>Your party name is {username} </p>;
 }
