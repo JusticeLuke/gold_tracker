de64e607bdc80a782e90cf5a7beb7d25238f074b
Luke Justice <jjusticeluke@gmail.com>
2022-04-26 13:07:30 -0400

    Add accounts folder to hold login/register componenets

--- gold_tracker/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from gold_tracker.accounts.api import RegisterAPI
-from knox import views as know_views
-
-urlpatterns = [
-    path("api/auth", include("knox.urls")),
-    path("api/auth/register", RegisterAPI.as_view()),
-]
---
+++ gold_tracker_ui/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoginForm } from "./accounts/LoginForm";
+import { Users } from "./Users";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
