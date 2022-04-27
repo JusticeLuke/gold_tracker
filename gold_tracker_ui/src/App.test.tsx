d6a001625aeeaf2f35da1a1c2744ac2ece0e0ae5
Luke Justice <jjusticeluke@gmail.com>
2022-04-26 22:58:20 -0400

    Remove unused files leftover from template

--- gold_tracker_ui/src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./components/App";
-
-test("renders learn react link", () => {
-  render(<App />);
-});
