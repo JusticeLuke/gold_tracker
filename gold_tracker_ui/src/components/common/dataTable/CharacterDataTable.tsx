4f21498d203c79af69e99242a21e6cdb87390195
Luke Justice <jjusticeluke@gmail.com>
2022-05-23 21:05:20 -0400

    Add data table for user partys and party characters

---
+++ gold_tracker_ui/src/components/common/dataTable/CharacterDataTable.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import EditIcon from "@mui/icons-material/Edit";
+import CommonButton from "../commonButton/CommonButton";
+import CharacterCard from "../../suite/inventoryComponents/CharacterCard";
+import AddIcon from "@mui/icons-material/AddCircleOutline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function CharacterDataTable(rows: any) {
+  const [character, setCharacter] = React.useState<any>();
+  const handleSubmit = (row: any) => {
+    setCharacter(row);
+  };
+  console.log(rows.rows);
+  if (rows.rows.length > 0) {
+    return (
+      <TableContainer sx={{ display: "inline-flex" }} component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Characters</TableCell>
+              <TableCell>
+                <CommonButton
+                  variant={"contained"}
+                  color={"primary"}
+                  title={"Add new character"}
+                  placement={"right-end"}
+                >
+                  <AddIcon />
+                </CommonButton>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.rows.map((row: any) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">
+                  <CommonButton
+                    variant={"contained"}
+                    color={"primary"}
+                    onClick={() => {
+                      handleSubmit(row);
+                    }}
+                  >
+                    <EditIcon />
+                  </CommonButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <CharacterCard row={character} />
+      </TableContainer>
+    );
+  } else {
+    return (
+      <Box sx={{ display: "block", textAlign: "center" }}>
+        <Typography>This party has no characters</Typography>{" "}
+        <CommonButton
+          variant={"contained"}
+          color={"primary"}
+          title={"Add new character"}
+          placement={"right-end"}
+        >
+          <AddIcon />
+        </CommonButton>
+      </Box>
+    );
+  }
+}
---
+++ gold_tracker_ui/src/components/common/dataTable/PartyDataTable.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import EditIcon from "@mui/icons-material/Edit";
+import CommonButton from "../commonButton/CommonButton";
+import { useNavigate } from "react-router-dom";
+import { getPartyCharacters } from "../../../actions/characterActions/CRUDCharacter";
+
+export default function PartyDataTable(rows: any) {
+  const navigate = useNavigate();
+  const editPartyClick = async (row: any) => {
+    localStorage.setItem("partyId", row.id);
+    await getPartyCharacters();
+    navigate("inventory");
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Party Name</TableCell>
+            <TableCell align="right">Copper</TableCell>
+            <TableCell align="right">Silver</TableCell>
+            <TableCell align="right">Gold</TableCell>
+            <TableCell align="center">Manage Partys</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.rows.map((row: any) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.anon_copper}</TableCell>
+              <TableCell align="right">{row.anon_silver}</TableCell>
+              <TableCell align="right">{row.anon_gold}</TableCell>
+              <TableCell align="center">
+                <CommonButton
+                  variant={"contained"}
+                  color={"primary"}
+                  onClick={() => editPartyClick(row)}
+                >
+                  <EditIcon />
+                </CommonButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
