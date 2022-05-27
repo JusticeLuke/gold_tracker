b5aac40968a33bd8886209857cd50b997b9e6606
Luke Justice <jjusticeluke@gmail.com>
2022-05-26 23:17:28 -0400

    Displays log

--- gold_tracker_ui/src/components/suite/inventoryComponents/LogCard.tsx
+++ gold_tracker_ui/src/components/suite/inventoryComponents/LogCard.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import * as React from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import BasicCard from "../../common/basicCard/BasicCard";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
 
 const LogCard = () => {
-  return <div>LogCard</div>;
+  const [log, setLog] = React.useState<any>();
+
+  React.useEffect(() => {
+    // Runs after the first render() lifecycle
+    setLog(localStorage.getItem("log"));
+  }, []);
+
+  const getLogData = () => {
+    let logData = [];
+    if (log != null) {
+      logData = JSON.parse(log);
+    }
+    return logData;
+  };
+  const getContent = () => {
+    const logData = getLogData();
+    console.log(logData);
+    return (
+      <List>
+        {logData.map((row: any) => (
+          <ListItem>
+            <ListItemText primary={row.entry}> </ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+  return (
+    <Grid item xs={4}>
+      <BasicCard
+        content={getContent()}
+        header={"Party Log"}
+        sx={{ mt: "5px" }}
+      />
+    </Grid>
+  );
 };
 
 export default LogCard;
