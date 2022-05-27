860f905ede3e4d0760e955cb2858e008f0bdaed5
Luke Justice <jjusticeluke@gmail.com>
2022-05-26 23:14:29 -0400

    Add get function for logs

--- gold_tracker_ui/src/actions/logActions/CRLog.tsx
+++ gold_tracker_ui/src/actions/logActions/CRLog.tsx
@@ -7,7 +7,7 @@ export async function createLog(data: any) {
   try {
     let token = localStorage.getItem("token");
     let partyId = localStorage.getItem("partyId");
-    const res = await fetch(`${endpoint}/party/${partyId}/log`, {
+    const res = await fetch(`${endpoint}/partys/${partyId}/log`, {
       method: "POST",
       headers: {
         Accept: "application/json; indent=4",
@@ -23,5 +23,48 @@ export async function createLog(data: any) {
   } catch (error) {
     console.log(error);
     return false;
+  }
+}
+
+export async function getLog() {
+  try {
+    let logArray = [];
+    let token = localStorage.getItem("token");
+    let partyId = localStorage.getItem("partyId");
+    const logRes = await fetch(`${endpoint}/partys/${partyId}/log`, {
+      method: "GET",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    let logJson = await logRes.json();
+    let pageNum = 2;
+    for (let x = 0; x < logJson.results.length; x++) {
+      logArray.push(logJson.results[x]);
+    }
+    while (logJson.next) {
+      let nextLogRes = await fetch(
+        `${endpoint}/partys/${partyId}/log?page=${pageNum}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      logJson = await nextLogRes.json();
+      pageNum++;
+      for (let x = 0; x < logJson.results.length; x++) {
+        logArray.push(logJson.results[x]);
+      }
+    }
+
+    localStorage.setItem("log", JSON.stringify(logArray));
+    return logJson;
+  } catch (error) {
+    console.log(error);
   }
 }
