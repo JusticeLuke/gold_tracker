95147364fe4b8438dcd895cbad792102f1569f93
Luke Justice <jjusticeluke@gmail.com>
2022-05-29 11:53:44 -0400

    Removed useEffect function

--- gold_tracker_ui/src/components/App.tsx
+++ gold_tracker_ui/src/components/App.tsx
@@ -2,22 +2,8 @@ import React, { useEffect } from "react";
 import Navbar from "./navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../actions/userActions/AuthProvider";
 
 function App() {
-  let auth = useAuth();
-  let navigate = useNavigate();
-
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      navigate("login");
-    } else {
-      auth.signin("", true);
-      navigate("partys");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return (
     <Grid container>
       <Navbar />
