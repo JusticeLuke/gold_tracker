240949299bc6b451a4b8bec6b583e8567715ebad
Luke Justice <jjusticeluke@gmail.com>
2022-05-29 12:01:45 -0400

    Remove useless imports

--- gold_tracker_ui/src/components/App.tsx
+++ gold_tracker_ui/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "./navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Grid from "@mui/material/Grid";
