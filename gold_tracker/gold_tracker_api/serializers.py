02174b48db652b1358fdbf20011c20cb65be5a9a
Lucas Justice <35172162+JusticeLuke@users.noreply.github.com>
2022-10-10 14:18:37 -0400

    Add validatibackend validation for wealth changes across all models

--- gold_tracker/gold_tracker_api/serializers.py
+++ gold_tracker/gold_tracker_api/serializers.py
@@ -1,8 +1,25 @@
+from email import message
+from wsgiref.validate import validator
 from django.contrib.auth.models import User, Group
 from gold_tracker.gold_tracker_api.models import Character, Log, Party
 from rest_framework import serializers
 
-#
+class WealthValidator():
+    def __init__(self, model):
+        self.model = model
+        self.message = model + " wealth values cannot be negative"
+
+    def __call__(self, attrs):
+        if self.model == "Log":
+            if attrs['gold'] < 0 or attrs['silver'] < 0 or attrs['copper'] < 0:
+                raise serializers.ValidationError(self.message)
+        elif self.model == "Party":
+            if attrs['anon_gold'] < 0 or attrs['anon_silver'] < 0 or attrs['anon_copper'] < 0:
+                raise serializers.ValidationError(self.message)
+        elif self.model == "Character":
+            if attrs["personal_gold"] < 0 or attrs["personal_silver"] or attrs["personal_copper"] < 0 or attrs["tribute_gold"] < 0 or attrs["tribute_silver"] < 0 or attrs["tribute_copper"] < 0:
+                raise serializers.ValidationError(self.message)
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
@@ -19,6 +36,7 @@ class PartySerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
         fields = ["id", "name", "anon_gold", "anon_silver", "anon_copper", "master"]
+        validators = [WealthValidator("Party")]
 
 
 class CharacterSerializer(serializers.ModelSerializer):
@@ -35,9 +53,12 @@ class CharacterSerializer(serializers.ModelSerializer):
             "tribute_copper",
             "party_id",
         ]
+        validators = [WealthValidator("Character")]
 
 
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
         fields = ["id", "entry", "gold", "silver", "copper", "party_id"]
+        validators = [WealthValidator("Log")]
+
